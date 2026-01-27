30094c6fc5d0e64f482ee9837334a55a67d6219f
tamerlan3211 <tamerlan3211@gmail.com>
2026-01-27 16:02:22 +0500

    Add Python f

---
+++ lab1/data_type.py
@@ -0,0 +1,24 @@
+
+# Строка
+name = "Alice"
+print("Name:", name, "Type:", type(name))
+
+# Целое число
+age = 25
+print("Age:", age, "Type:", type(age))
+
+# Вещественное число (float)
+height = 1.68
+print("Height:", height, "Type:", type(height))
+
+# Булев тип
+is_student = True
+print("Is student?", is_student, "Type:", type(is_student))
+
+# Список
+fruits = ["apple", "banana", "cherry"]
+print("Fruits:", fruits, "Type:", type(fruits))
+
+# Простая операция с числами
+sum_age_height = age + height
+print("Age + Height =", sum_age_height, "Type:", type(sum_age_height))
